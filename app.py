a454ef95a876277b5a76156f25d52ca28953a6fe
Fernando Gutierrez <fernando.gutierrez@gmail.com>
2024-07-08 10:34:54 -0400

    Refactor: Move DB functions to a different file

--- app.py
+++ app.py
@@ -18,35 +18,10 @@ import sqlite3
 from datetime import datetime, time
 
 from MultiColorProgressBar import MultiColorProgressBar
+from db import init_pomodoro_db, insert_pomodoro_session
 from motivation import get_motivational_phrase
 
 from sound import play_melody
-
-
-def init_pomodoro_db():
-    # Function to create/connect to a SQLite database and create the table if it doesn't exist
-    conn = sqlite3.connect("pomodoro_sessions.db")
-    c = conn.cursor()
-    c.execute(
-        """CREATE TABLE IF NOT EXISTS session_feedback
-                 (start_time TEXT, end_time TEXT, feeling TEXT)"""
-    )
-    conn.commit()
-    conn.close()
-
-
-def insert_pomodoro_session(start_time, end_time, feeling):
-    if not start_time:
-        return  # Do not proceed if start_time is not set
-    # Function to insert a session record into the database
-    conn = sqlite3.connect("pomodoro_sessions.db")
-    c = conn.cursor()
-    c.execute(
-        "INSERT INTO session_feedback (start_time, end_time, feeling) VALUES (?, ?, ?)",
-        (start_time, end_time, feeling),
-    )
-    conn.commit()
-    conn.close()
 
 
 class XbitoPomodoro(QMainWindow):
---
+++ db.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+
+def init_pomodoro_db():
+    # Function to create/connect to a SQLite database and create the table if it doesn't exist
+    conn = sqlite3.connect("pomodoro_sessions.db")
+    c = conn.cursor()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS session_feedback
+                 (start_time TEXT, end_time TEXT, feeling TEXT)"""
+    )
+    conn.commit()
+    conn.close()
+
+
+def insert_pomodoro_session(start_time, end_time, feeling):
+    if not start_time:
+        return  # Do not proceed if start_time is not set
+    # Function to insert a session record into the database
+    conn = sqlite3.connect("pomodoro_sessions.db")
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO session_feedback (start_time, end_time, feeling) VALUES (?, ?, ?)",
+        (start_time, end_time, feeling),
+    )
+    conn.commit()
+    conn.close()
